8ed130713c3e802d589b61d854a127640aafb135
JOY254 <nyorgijoy@gmail.com>
2023-11-07 23:42:19 -0600

    added home function

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,2 +1,11 @@
 import React from "react"
 import pina from "../image/pina.png"
+
+function Home() {
+    return (
+        <div className= "Home-div">
+        <div className= "Home">  
+        </div>
+        </div>
+    )
+}
