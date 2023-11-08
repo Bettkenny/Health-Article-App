97fb62a8aec3f77e15f6be99c20c43e6dbed3d7a
JOY254 <nyorgijoy@gmail.com>
2023-11-08 07:14:35 -0600

    added h1

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
     return (
         <div className= "Home-div">
         <div className= "Home">  
+        <h1 className="home-par" > HEALTH-ARTICLE-APP</h1>
         </div>
         </div>
     )
